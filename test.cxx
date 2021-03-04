5f35075c3ccf919cfb5f105631c920d3d2e77d57
wangzhen <wztbxl@wangzhendeMacBook-Pro.local>
2021-03-03 19:51:34 -0500

    change the include method, now it works, loop all channel to debug

--- test.cxx
+++ test.cxx
@@ -20,12 +20,24 @@ void test( string name = "FEE2Slot_Layer1.dat" )
     int FEE, ALTRO, Channel;
     for ( int i = 0; i < 10; i++)
     {
-        FEE = (int)gRandom->Uniform(1,16);
-        ALTRO = FEE*2 + (int)gRandom->Uniform(0.5,1.5);
-        Channel = (int)gRandom->Uniform(1,16);
-        int Strip = Layer1.GetStripNumber(FEE,ALTRO,Channel);
-        cout << "FEE is " << FEE << " ALTRO is " << ALTRO << " Channel is " << Channel << endl;
-        cout << " #Strip is " << Strip << endl;  
+        // FEE = (int)gRandom->Uniform(1,16);
+        // ALTRO = FEE*2 + (int)gRandom->Uniform(0.5,1.5);
+        // Channel = (int)gRandom->Uniform(1,16);
+        for ( int i = 1; i <= 15; i++ )
+        {
+            FEE = i;
+            for ( int j = 0; j <= 1; j++ )
+            {
+                ALTRO = FEE*2+j;
+                for ( int k = 0; k <= 15; k++ )
+                {
+                    Channel = k;
+                    int Strip = Layer1.GetStripNumber(FEE,ALTRO,Channel);
+                    cout << "FEE is " << FEE << " ALTRO is " << ALTRO << " Channel is " << Channel << endl;
+                    cout << " #Strip is " << Strip << endl; 
+                }
+            }
+        } 
     }
     
 
